e0f2e7f19ddc7ed3b72d73df3ec209e29fbfcd52
vuxuanson61HT <93304850+vuxuanson61HT@users.noreply.github.com>
2021-11-02 21:26:33 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,271 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Login</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+        <link rel="icon" href="images/favicon.png" type="image/x-icon">
+        
+        <!-- Google Fonts -->	
+        <link href="https://fonts.googleapis.com/css?family=Lato:300,300i,400,400i,700,700i,900,900i%7CMerriweather:300,300i,400,400i,700,700i,900,900i" rel="stylesheet">
+        
+        <!-- Bootstrap Stylesheet -->	
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        
+        <!-- Font Awesome Stylesheet -->
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+            
+        <!-- Custom Stylesheets -->	
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" id="cpswitch" href="css/orange.css">
+        <link rel="stylesheet" href="css/responsive.css">
+    </head>
+    
+    
+    <body>
+    
+        <!--====== LOADER =====-->
+        <div class="loader"></div>
+    
+    
+    	<!--======== SEARCH-OVERLAY =========-->       
+        <div class="overlay">
+            <a href="javascript:void(0)" id="close-button" class="closebtn">&times;</a>
+            <div class="overlay-content">
+                <div class="form-center">
+                    <form>
+                    	<div class="form-group">
+                        	<div class="input-group">
+                        		<input type="text" class="form-control" placeholder="Search..." required />
+                            	<span class="input-group-btn"><button type="submit" class="btn"><span><i class="fa fa-search"></i></span></button></span>
+                            </div><!-- end input-group -->
+                        </div><!-- end form-group -->
+                    </form>
+                </div><!-- end form-center -->
+            </div><!-- end overlay-content -->
+        </div><!-- end overlay -->
+        
+        
+        <!--============= TOP-BAR ===========-->
+        <div id="top-bar" class="tb-text-white">
+            <div class="container">
+                <div class="row">          
+                    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+                        <div id="info">
+                            <ul class="list-unstyled list-inline">
+                                <li><span><i class="fa fa-map-marker"></i></span>TÂY SƠN, ĐỐNG ĐA, HÀ NỘI</li>
+                                <li><span><i class="fa fa-phone"></i></span>0987654321</li>
+                            </ul>
+                        </div><!-- end info -->
+                    </div><!-- end columns -->
+                    
+                    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+                        <div id="links">
+                            <ul class="list-unstyled list-inline">
+                                <li><a href="login.html"><span><i class="fa fa-lock"></i></span>ĐĂNG NHẬP</a></li>
+                                <li><a href="registration.html"><span><i class="fa fa-plus"></i></span>ĐĂNG KÍ</a></li>
+                            </ul>
+                        </div><!-- end links -->
+                    </div><!-- end columns -->				
+                </div><!-- end row -->
+            </div><!-- end container -->
+        </div><!-- end top-bar -->
+
+        <nav class="navbar navbar-default main-navbar navbar-custom navbar-white" id="mynavbar-1">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" id="menu-button">
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>                        
+                    </button>
+                    <div class="header-search hidden-lg">
+                    	<a href="javascript:void(0)" class="search-button"><span><i class="fa fa-search"></i></span></a>
+                    </div>
+                    <a href="#" class="navbar-brand"><span><i class="fa fa-plane"></i>STAR</span>TRAVELS</a>
+                </div><!-- end navbar-header -->
+                
+                <div class="collapse navbar-collapse" id="myNavbar1">
+                    <ul class="nav navbar-nav navbar-right navbar-search-link">
+                        <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Home<span><i class="fa fa-angle-down"></i></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="index.html">Main Homepage</a></li>
+                                <li><a href="hotel-homepage.html">Hotel Homepage</a></li>
+                                <li><a href="tour-homepage.html">Tour Homepage</a></li>
+                            </ul>			
+                        </li>
+                        
+                        <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Hotels<span><i class="fa fa-angle-down"></i></span></a>
+                            <ul class="dropdown-menu">
+                            	<li><a href="hotel-homepage.html">Hotel Homepage</a></li>
+                                <li><a href="hotel-listing-left-sidebar.html">List View Left Sidebar</a></li>
+                            </ul>			
+                        </li>
+                        <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Tours<span><i class="fa fa-angle-down"></i></span></a>
+                            <ul class="dropdown-menu">
+                            	<li><a href="tour-homepage.html">Tour Homepage</a></li>
+                                <li><a href="tour-listing-left-sidebar.html">List View Left Sidebar</a></li>
+                            </ul>			
+                        </li>
+                        <li><a href="javascript:void(0)" class="search-button"><span><i class="fa fa-search"></i></span></a></li>
+                    </ul>
+                </div><!-- end navbar collapse -->
+            </div><!-- end container -->
+        </nav><!-- end navbar -->
+        
+        <div class="sidenav-content">
+            <div id="mySidenav" class="sidenav" >
+                <h2 id="web-name"><span><i class="fa fa-plane"></i></span>Star Travel</h2>
+
+                <div id="main-menu">
+                	<div class="closebtn">
+                        <button class="btn btn-default" id="closebtn">&times;</button>
+                    </div><!-- end close-btn -->
+                    
+                    <div class="list-group panel">
+                    
+                        <a href="#home-links" class="list-group-item" data-toggle="collapse" data-parent="#main-menu"><span><i class="fa fa-home link-icon"></i></span>Home<span><i class="fa fa-chevron-down arrow"></i></span></a>
+                        <div class="collapse sub-menu" id="home-links">
+                            <a href="index.html" class="list-group-item">Main Homepage</a>
+                            <a href="hotel-homepage.html" class="list-group-item">Hotel Homepage</a>
+                            <a href="tour-homepage.html" class="list-group-item">Tour Homepage</a>
+                        </div><!-- end sub-menu -->
+                        
+                        <a href="#hotels-links" class="list-group-item" data-toggle="collapse" data-parent="#main-menu"><span><i class="fa fa-building link-icon"></i></span>Hotels<span><i class="fa fa-chevron-down arrow"></i></span></a>
+                        <div class="collapse sub-menu" id="hotels-links">
+                            <a href="hotel-homepage.html" class="list-group-item">Hotel Homepage</a>
+                            <a href="hotel-listing-left-sidebar.html" class="list-group-item">List View Left Sidebar</a>
+                        </div><!-- end sub-menu -->
+                        
+                        <a href="#tours-links" class="list-group-item" data-toggle="collapse" data-parent="#main-menu"><span><i class="fa fa-globe link-icon"></i></span>Tours<span><i class="fa fa-chevron-down arrow"></i></span></a>
+
+                        <div class="collapse sub-menu" id="tours-links">
+                            <a href="tour-homepage.html" class="list-group-item">Tour Homepage</a>
+                            <a href="tour-listing-left-sidebar.html" class="list-group-item">List View Left Sidebar</a>
+                        </div><!-- end sub-menu -->
+                        
+                    </div><!-- end list-group -->
+                </div><!-- end main-menu -->
+            </div><!-- end mySidenav -->
+        </div><!-- end sidenav-content -->
+        
+        <section class="page-cover" id="cover-login">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-12">
+                    	<h1 class="page-title">TRANG ĐĂNG NHẬP</h1>
+                        <ul class="breadcrumb">
+                            <li><a href="index.html">Home</a></li>
+                        </ul>
+                    </div><!-- end columns -->
+                </div><!-- end row -->
+            </div><!-- end container -->
+        </section><!-- end page-cover -->
+        
+        <section class="innerpage-wrapper">
+        	<div id="login" class="innerpage-section-padding">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-12">
+                        
+                        	<div class="flex-content">
+                                <div class="custom-form custom-form-fields">
+                                    <h3>ĐĂNG NHẬP</h3>
+                                    <form>
+                                            
+                                        <div class="form-group">
+                                             <input type="text" class="form-control" placeholder="Tên đăng nhập"  required/>
+                                             <span><i class="fa fa-user"></i></span>
+                                        </div>
+                                        
+                                        <div class="form-group">
+                                             <input type="password" class="form-control" placeholder="Mật khẩu"  required/>
+                                             <span><i class="fa fa-lock"></i></span>
+                                        </div>
+                                        
+                                        <div class="checkbox">
+                                             <label><input type="checkbox"> Nhớ mật khẩu</label>
+                                        </div>
+                                        
+                                        <button class="btn btn-orange btn-block">Đăng nhập</button>
+                                    </form>
+                                    
+                                    <div class="other-links">
+                                    	<p class="link-line">Tài khoản mới ? <a href="#">Đăng ký</a></p>
+                                    </div><!-- end other-links -->
+                                </div><!-- end custom-form -->
+                                
+                                <div class="flex-content-img custom-form-img">
+                                    <img src="images/login.jpg" class="img-responsive" alt="registration-img" />
+                                </div><!-- end custom-form-img -->
+                            </div><!-- end form-content -->
+                        </div><!-- end columns -->
+                    </div><!-- end row -->
+                </div><!-- end container -->         
+            </div><!-- end login -->
+        </section><!-- end innerpage-wrapper -->
+        
+        <section id="footer" class="ftr-heading-o ftr-heading-mgn-1">
+        
+            <div id="footer-top" class="banner-padding ftr-top-black ftr-text-white">
+                <div class="container">
+                    <div class="row">
+						
+                        <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 footer-widget ftr-contact">
+                            <h3 class="footer-heading">LIÊN HỆ CHÚNG TÔI</h3>
+                            <ul class="list-unstyled">
+                            	<li><span><i class="fa fa-map-marker"></i></span>TÂY SƠN, ĐỐNG ĐA, HÀ NỘI</li>
+                            	<li><span><i class="fa fa-phone"></i></span>0987654321</li>
+                                <li><span><i class="fa fa-envelope"></i></span>0987654321@gmail.com</li>
+                            </ul>
+                        </div><!-- end columns -->
+                        
+                        <div class="col-xs-12 col-sm-6 col-md-2 col-lg-2 footer-widget ftr-links">
+                            <h3 class="footer-heading">CÔNG TY</h3>
+                            <ul class="list-unstyled">
+                            	<li><a href="index.html">Home</a></li>
+                                <li><a href="hotel-detail-left-sidebar.html">Hotel</a></li>
+                                <li><a href="tour-detail-left-sidebar.html">Tours</a></li>
+                            </ul>
+                        </div><!-- end columns -->
+                        
+                        <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 footer-widget ftr-links ftr-pad-left">
+                        	<h3 class="footer-heading">TÀI NGUYÊN</h3>
+                            <ul class="list-unstyled">
+                                <li><a href="login.html">Đăng nhập</a></li>
+                                <li><a href="registration.html">Đăng ký</a></li>
+                            </ul>
+                        </div><!-- end columns -->
+
+                        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 footer-widget ftr-about">
+                            <h3 class="footer-heading">VỀ CHÚNG TÔI</h3>
+                            <p>Đảm bảo cho mọi người những chuyến du lịch sang chảnh và đảm bảo nhất</p>
+                            
+                        </div><!-- end columns -->
+
+                    </div><!-- end row -->
+                </div><!-- end container -->
+            </div><!-- end footer-top -->
+
+            <div id="footer-bottom" class="ftr-bot-black">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6" id="copyright">
+                            <p>© 2021 <a href="#">Chấp cánh ước mơ</a></p>
+                        </div><!-- end columns -->
+                        
+                        
+                    </div><!-- end row -->
+                </div><!-- end container -->
+            </div><!-- end footer-bottom -->
+            
+        </section><!-- end footer -->
+        
+        
+        <!-- Page Scripts Starts -->
+        <script src="js/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/custom-navigation.js"></script>
+        <!-- Page Scripts Ends -->
+    </body>
+</html>
