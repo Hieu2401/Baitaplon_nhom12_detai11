ba994065be7c01226faca4714d10d4984abd3516
Hieu2401 <92858492+Hieu2401@users.noreply.github.com>
2021-11-04 23:50:25 +0700

    Update starvel.sql

--- starvel.sql
+++ starvel.sql
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `admin`
+-- Cấu trúc bảng cho bảng `admin
 --
 
 CREATE TABLE `admin` (
